e28d16b79140e285746a21d60504dd8561d9026f
Sandeep <sandeep167kumar@gmail.com>
2023-10-03 08:26:16 +0530

    added new file DigitButton

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-
+import { digitButton } from './DigitButton';
+import { useReducer } from 'react';
 import './App.css';
 
+export const ACTION = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose_operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUTE: 'evalute'
+}
+function reducer (state, {type, payload}) {
+  switch(type) {
+    case ACTION.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ""}${payload.digit}`
+      }
+  }
+  
+}
+
 function App() {
+  const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer())
+
+  // dispatch({type: ACTION.ADD_DIGIT, payload: {digit: 1}})
   return (
     <div className="calculator_grid">
     <div className="output">
-      <div className="previous_operand"></div>
-      <div className="current_operand"></div>
+      <div className="previous_operand">{previousOperand} {operation}</div>
+      <div className="current_operand">{currentOperand}</div>
     </div>
       
       <button className="span_two">AC</button>
       <button>DEL</button>
       <button>+</button>
-      <button>1</button>
+      <digitButton dispatch={dispatch}>1</digitButton>
       <button>2</button>
       <button>3</button>
       <button>*</button>
